ed267f730f5e3e06b0bab7ec1041e23b0729d22a
ryanbmarx <ryanbmarx@gmail.com>
2018-06-25 21:17:58 -0500

    Add basic build deps

---
+++ Gruntfile.js
@@ -0,0 +1,117 @@
+module.exports = function(grunt) {
+  var config = {};
+
+  // Put your JavaScript library dependencies here. e.g. jQuery, underscore,
+  // etc.
+  // You'll also have to install them using a command similar to:
+  //     npm install --save jquery
+  var VENDOR_LIBRARIES = [
+  ];
+
+  config.browserify = {
+    options: {
+      browserifyOptions: {
+        debug: true
+      }
+    },
+    app: {
+      src: ['js/src/app.js'],
+      dest: 'js/app.min.js',
+      options: {
+        plugin: [
+          [
+            'minifyify', {
+              map: 'app.min.js.map',
+              output: './js/app.min.js.map'
+            }
+          ]
+        ],
+        transform: [
+          [
+            'babelify', {
+              "presets": [
+                ["env", {
+                  "targets": {
+                    "browsers": ["last 2 versions", "ie >= 11"]
+                  }
+                }]
+              ]
+            }
+          ]
+        ]
+      }
+    }
+  };
+
+  // Check if there are vendor libraries and build a vendor bundle if needed
+  if (VENDOR_LIBRARIES.length) {
+    config.browserify.app.options = config.browserify.app.options || {};
+    config.browserify.app.options.exclude = VENDOR_LIBRARIES;
+
+    config.browserify.vendor = {
+      src: [],
+      dest: 'js/vendor.min.js',
+      options: {
+        plugin: [
+          [
+            'minifyify', {
+              map: 'vendor.min.js.map',
+              output: './js/vendor.min.js.map'
+            }
+          ]
+        ],
+        require: VENDOR_LIBRARIES
+      }
+    };
+  }
+
+  config.sass = {
+    options: {
+      outputStyle: 'compressed',
+      sourceMap: true,
+      includePaths: [ 'sass/', 'node_modules/trib-styles/sass/', '_blueprint/base-sass/' ]
+    },
+    app: {
+      files: {
+        'css/styles.css': 'sass/styles.scss'
+      }
+    }
+  };
+
+  config.postcss = {
+    options: {
+      processors: [
+        require('autoprefixer')() 
+      ]
+    },
+    dist: {
+      src: 'css/*.css'
+    }
+  }
+
+  config.watch = {
+    sass: {
+      files: ['sass/**/*.scss'],
+      tasks: ['sass', 'postcss']
+    },
+    js: {
+      files: ['js/src/**/*.js'],
+      tasks: ['browserify:app']
+    }
+  };
+
+  grunt.initConfig(config);
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+
+  var defaultTasks = [];
+
+  defaultTasks.push('sass');
+  defaultTasks.push('browserify');
+  defaultTasks.push('postcss');
+
+  grunt.registerTask('default', defaultTasks);
+};
